c66de42550d4e3a23268ac5b774967449c719e18
nazarovman2024 <nazarovman2024@gmail.com>
2025-03-09 22:35:10 +0300

    250309_JPP_2.1.2_0

---
+++ src/main/java/Cat.java
@@ -0,0 +1,11 @@
+public class Cat {
+    private String name = "DefaultCat";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
